48e5fb5bf267f9ad6c10e81bd9ee80f380bbdd9e
Daryl Wada <daryl.wada@gmail.com>
2018-06-13 13:18:35 -0700

    Added a move method to the Car class that updates its location according to speed and direction. #3

--- main.js
+++ main.js
@@ -9,13 +9,28 @@ class Car {
     this.direction = direction
     this.$img.className = direction
   }
+  move() {
+    switch (this.direction) {
+      case 'north':
+        this.location[1] += this.speed
+        break
+      case 'south':
+        this.location[1]-= this.speed
+        break
+      case 'east':
+        this.location[0] += this.speed
+        break
+      case 'west':
+        this.location[0] -= this.speed
+    }
+  }
 }
 
 var $img = document.createElement('img')
 $img.src = 'car-top-down.png'
 document.body.appendChild($img)
 
-var car = new Car($img)
+var car = new Car($img, 1, 'east', [0, 0])
 
 document.addEventListener('keydown', (event) => {
   switch (event.key) {

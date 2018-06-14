3d9c389302637a9e0bdc057ca098a2a735d9b66e
Daryl Wada <daryl.wada@gmail.com>
2018-06-14 12:41:15 -0700

    Refactored to minimize side effects. #9

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 class Car {
-  constructor($img, speed, direction, location) {
-    this.$img = $img
+  constructor($carImage, speed, direction, location) {
+    this.$carImage = $carImage
     this.speed = speed
     this.direction = direction
     this.location = location
     this.started = false
     this.interval = null
   }
+
   turn(direction) {
     this.direction = direction
-    this.$img.className = direction
+    this.$carImage.className = direction
   }
+
   move() {
     switch (this.direction) {
       case 'north':
@@ -25,12 +27,15 @@ class Car {
       case 'west':
         this.location[0] -= this.speed
     }
-    this.$img.style = "top: " + this.location[1] + "px; " + "left: " + this.location[0] + "px;"
+    this.$carImage.style.top = `${this.location[1]}px`
+    this.$carImage.style.left = `${this.location[0]}px`
   }
+
   start() {
     this.interval = setInterval(() => this.move(), 16)
     this.started = true
   }
+
   stop() {
     clearInterval(this.interval)
     this.started = false
@@ -38,12 +43,11 @@ class Car {
 }
 
 class RaceCar extends Car {
-  constructor($img, speed, direction, location, nitros) {
-    super($img, speed, direction, location)
+  constructor($carImage, speed, direction, location, nitros) {
+    super($carImage, speed, direction, location)
     this.nitros = nitros
-    this.started = false
-    this.interval = null
   }
+
   nitro() {
     if (this.nitros > 0) {
       this.speed *= 2
@@ -52,34 +56,35 @@ class RaceCar extends Car {
   }
 }
 
-function updateCarObj(type) {
-  var $img = document.querySelector('img')
-  car = type === 'streetcar'
-    ? new Car($img, 5, 'east', [0, 0])
-    : new RaceCar($img, 5, 'east', [0, 0], 3)
+function createCar($carImage, carType) {
+  return carType === 'streetcar'
+    ? new Car($carImage, 5, 'east', [0, 0])
+    : new RaceCar($carImage, 5, 'east', [0, 0], 3)
 }
 
-function renderCar(type) {
-  var $img = document.createElement('img')
-  $img.className = 'east'
-  $img.style = "top: 0px; left: 0px;"
-  $img.src = type === 'streetcar'
+function createCarImage(carType) {
+  var $carImage = document.createElement('img')
+
+  $carImage.className = 'east'
+  $carImage.style = "top: 0px; left: 0px;"
+  $carImage.src = carType === 'streetcar'
     ? 'car-top-down.png'
     : 'racecar-top-down.png'
-  return $img
-}
 
-function renderApp(type) {
-  document.body.innerHTML = ''
-  document.body.appendChild(renderCar(type))
+  return $carImage
 }
 
 var car = {}
 
 document.querySelectorAll('button').forEach(btn => {
   btn.addEventListener('click', (event) => {
-    renderApp(event.target.value)
-    updateCarObj(event.target.value)
+    var carType = event.target.value
+    var $carImage = createCarImage(carType)
+
+    car = createCar($carImage, carType)
+
+    document.body.innerHTML = ''
+    document.body.appendChild($carImage)
   })
 })
 
